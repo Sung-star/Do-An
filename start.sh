09717c2f64d920ea54a448997a30f43750dc39ff
Ta Van Hoai Sung <xuanta142005@gmail.com>
2025-10-12 15:43:05 +0700

    Fix chạy Laravel trên Railway - thêm start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# Cài đặt dependency nếu chưa có
 composer install --no-dev --optimize-autoloader
-# Tạo cache Laravel
 php artisan config:cache
 php artisan route:clear
 php artisan view:clear
-# Chạy server Laravel
-php artisan serve --host=0.0.0.0 --port=10000
+php artisan migrate --force
+php artisan serve --host=0.0.0.0 --port=$PORT
