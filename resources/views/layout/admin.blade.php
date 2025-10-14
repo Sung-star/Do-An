28cfc35062f2ae268e7ab6d741048be1f898acde
Ta Van Hoai Sung <xuanta142005@gmail.com>
2025-10-14 09:07:07 +0700

    fix: update env and admin layout for css issue

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -10,7 +10,7 @@
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+@vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
